75fe05e153731aff464fcd6706653a5f88b37f41
Mike Davis <davism84@gmail.com>
2022-05-24 16:45:38 -0400

    added support for facet ranges; also moved facet info into config.js

--- components/core/Facets.jsx
+++ components/core/Facets.jsx
@@ -3,25 +3,28 @@ import {
   Facet
 } from "@elastic/react-search-ui";
 
-
+import { MultiCheckboxFacet } from "@elastic/react-search-ui-views";
 
 const Facets = props => {
 
-//console.log('FACETS', props.fields)
+  console.log('FACETS comp', props)
   return (
-      props.fields.filter(field => field.facet.active === true)
-        .map((field) => {
+      <div>
+      {Object.entries(props.facets)
+        .map((facet) => {
         return (
          <Facet
-          key={field["_uid"]}
-          field={field["field"]}
-          label={field["label"]}
-          filterType={field["facet"]["filterType"]}
-          isFilterable={field["facet"]["isFilterable"]}
+          key={facet[0]}
+          field={facet[0]}
+          label={facet[1]["label"]}
+          filterType={facet[1]["filterType"]}
+          isFilterable={facet[1]["isFilterable"]}
+          //view={MultiCheckboxFacet}
         />
         );
       })
-  );
+      }
+  </div>);
 };
 
 export default Facets;
