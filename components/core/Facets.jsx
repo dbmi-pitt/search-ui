f6150c29a0a1efcae693dfc97cbedd77acf9bb33
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-25 13:34:24 -0400

    Add data-js-facets to Facets.jsx - #136

--- components/core/Facets.jsx
+++ components/core/Facets.jsx
@@ -5,14 +5,16 @@ import CollapsableFacet from "./CollapsableFacet";
 const Facets = ({fields, filters}) => {
     log.info('FACETS component props', fields, filters)
 
-    return (<>
-        {Object.entries(fields.facets)
-            .map(facet => (<CollapsableFacet
-                key={facet[0]}
-                facet={facet}
-                fields={fields}
-                filters={filters}/>))}
-    </>)
+    return (
+        <div data-js-facets>
+            {Object.entries(fields.facets)
+                .map(facet => (<CollapsableFacet
+                    key={facet[0]}
+                    facet={facet}
+                    fields={fields}
+                    filters={filters}/>))}
+        </div>
+    )
 }
 
 export default Facets;
