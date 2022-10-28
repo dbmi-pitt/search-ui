948c2dd96fe7cdd8dfeb91433076c459c3cec0ce
SamSedivy <sls325@pitt.edu>
2022-10-28 11:28:24 -0400

    Update clear filters button styles

--- components/core/ClearSearchBox.js
+++ components/core/ClearSearchBox.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {withSearch} from "@elastic/react-search-ui";
+import styles from '../../css/clearSearchBox.module.css'
+import {ArrowCounterclockwise} from 'react-bootstrap-icons'
+
 
 function ClearSearchBox({setSearchTerm}, shouldClearFilters) {
     return (
         <div className="sui-search-box__close">
-            <button className="btn btn-link" onClick={() => setSearchTerm("", {shouldClearFilters: true})}>Clear filters
+            <button className={`${styles.clearSearchBox} mt-2 btn px-4 rounded-pill shadow`} onClick={() => setSearchTerm("", {shouldClearFilters: true})}>
+                <ArrowCounterclockwise/>{' '}
+                Clear filters
             </button>
         </div>
     );
