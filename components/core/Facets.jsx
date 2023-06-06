089433639752c5019ca9fc09f1f9d8df1e9fd896
Tyler Madonna <tjm159@pitt.edu>
2023-06-06 10:08:04 -0400

    Refactor facets to use reusable components

--- components/core/Facets.jsx
+++ components/core/Facets.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
-import log from "loglevel"
-import CollapsableFacet from "./CollapsableFacet";
-import DateRangeFacet from "./DateRangeFacet";
+import React, { Fragment } from "react";
+import log from "loglevel";
+import CollapsableCheckboxFacet from "./CollapsableCheckboxFacet";
+import CollapsableDateRangeFacet from "./CollapsableDateRangeFacet";
 
 const Facets = ({fields, filters, transformFunction, clearInputs}) => {
-    log.info('FACETS component props', fields, filters)
+    log.info("FACETS component props", fields, filters);
+    
+    const conditionalFacets = fields.conditionalFacets;
+
+    function formatVal(id) {
+        return id.replace(/\W+/g, "")
+    }
+
+    function isConditionalFacet(facetKey) {
+        return conditionalFacets.hasOwnProperty(facetKey)
+    }
+
+    function getConditionalFacet(facetKey) {
+        return conditionalFacets[facetKey]
+    }
+
+    function isFacetVisible(facetKey) {
+        let result = true
+        if (isConditionalFacet(facetKey)) {
+            const predicate = getConditionalFacet(facetKey)
+            if (filters) {
+                result = predicate({filters})
+            } else {
+                result = false
+            }
+        }
+        return result
+    }
 
     return (<>
         {Object.entries(fields.facets)
             .map(facet => {
-                if (facet[1].uiType === 'daterange') {
-                    return <DateRangeFacet 
+                if (!isFacetVisible(facet[0])) {
+                    return <Fragment key={facet[0]}></Fragment>
+                }
+                
+                if (facet[1].uiType === "daterange") {
+                    return <CollapsableDateRangeFacet
                         key={facet[0]}
-                        filters={filters}
                         facet={facet}
-                        clearInputs={clearInputs} />
+                        clearInputs={clearInputs}
+                        formatVal={formatVal} />
                 } else {
-                    return <CollapsableFacet
+                    return <CollapsableCheckboxFacet
                         key={facet[0]}
                         facet={facet}
-                        fields={fields}
-                        filters={filters}
-                        transformFunction={transformFunction} />
+                        transformFunction={transformFunction}
+                        formatVal={formatVal} />
                 }
             }
         )}
