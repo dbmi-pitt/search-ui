e10001c65bbd3fb4903e2d5e266f9680c7a52620
Tyler Madonna <tjm159@pitt.edu>
2024-09-24 10:32:47 -0400

    Removing custom search driver class

--- components/core/SearchUIContainer.jsx
+++ components/core/SearchUIContainer.jsx
@@ -1,10 +1,9 @@
 import { SearchProvider, WithSearch } from '@elastic/react-search-ui'
-import SearchUISearchDriver from '../../packages/search-api-connector/SearchDriver'
 import { SearchUIProvider } from './SearchUIContext'
 
 function SearchUIContainer({ config, name, authState, children }) {
     return (
-        <SearchProvider config={config} driver={new SearchUISearchDriver(config)}>
+        <SearchProvider config={config}>
             <WithSearch mapContextToProps={({ filters, wasSearched, rawResponse }) => ({ filters, wasSearched, rawResponse })} >
                 {({ filters, wasSearched, rawResponse }) => {
                     return <SearchUIProvider name={name} authState={authState}>{children}</SearchUIProvider>
